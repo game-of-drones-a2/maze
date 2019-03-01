fab3ce8be92450221a42f1265d68aa2d629bde4c
Chrisini <c_b@live.at>
2019-03-01 01:33:56 +0100

    added comments

--- maze_code_v4/testing.ino
+++ maze_code_v4/testing.ino
@@ -6,9 +6,12 @@ void testing_routines() {
   
 void testing_letterlist(){
   letter_list = "LLBSBLLLLRSRBLLBLLRSLLLRBLRLLBLLBLLLLBRLBLLLBRRLL";
+  Serial.print("Test letter list: ");
   Serial.println(letter_list);
   transfer_table();
+  Serial.print("Test shorten: ");
   Serial.print(letter_list);
+  letter_list = ""; // working?
 }
 
 void testing_leds(){
